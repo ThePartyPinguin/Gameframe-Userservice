241f45d14054f53d46449c312bbfd138dce8426e
Joris Eijmberts <joriseijmberts@hotmail.nl>
2019-04-07 22:42:07 +0200

    updated gitignore and added project

---
+++ src/main/java/userservice/resource/UserController.java
@@ -0,0 +1,24 @@
+package userservice.resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/echoStudentName/{name}")
+    public String echoStudentName(@PathVariable(name = "name") String name)
+    {
+        return "hello  <strong style=\"color: red;\">" + name + " </strong> Responsed on : " + new Date();
+    }
+
+    @RequestMapping(value = "/getStudentDetails/{name}")
+    public String getStudentDetails(@PathVariable(name = "name") String name)
+    {
+        return "Here are the student details for: " + name;
+    }
+
+}
