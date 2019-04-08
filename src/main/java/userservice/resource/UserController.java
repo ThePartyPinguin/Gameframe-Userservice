4595c3b6b5e3b2560e6edfe985f359fa84659e29
Joris Eijmberts <joriseijmberts@hotmail.nl>
2019-04-08 16:33:51 +0200

    Updated test endpoint

--- src/main/java/userservice/resource/UserController.java
+++ src/main/java/userservice/resource/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
+@RequestMapping("/public")
 public class UserController {
 
     @RequestMapping(value = "/echoStudentName/{name}")
